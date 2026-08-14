184ff86de05e1e03a77724424ebc58633aa0a7b9
yumeerin <chan.misaki.y@gmail.com>
2026-08-14 19:41:56 +0000

    power: reset/supply: Import Xiaomi fastboot and battery configs

--- drivers/power/reset/qcom-reboot-reason.c
+++ drivers/power/reset/qcom-reboot-reason.c
@@ -17,10 +17,13 @@
 #include <linux/nvmem-consumer.h>
 #include <linux/of_device.h>
 #include <linux/of_platform.h>
+#include <linux/panic_notifier.h>
+
 
 struct qcom_reboot_reason {
 	struct device *dev;
 	struct notifier_block reboot_nb;
+	struct notifier_block panic_nb;
 	struct nvmem_cell *nvmem_cell;
 	struct poweroff_reason *reasons;
 };
@@ -58,6 +61,20 @@ static const struct of_device_id of_qcom_reboot_reason_match[] = {
 	{},
 };
 MODULE_DEVICE_TABLE(of, of_qcom_reboot_reason_match);
+static struct poweroff_reason reasons[] = {
+	{ "recovery",			0x01 },
+	{ "bootloader",			0x02 },
+	{ "rtc",			0x03 },
+	{ "dm-verity device corrupted",	0x04 },
+	{ "dm-verity enforcing",	0x05 },
+	{ "keys clear",			0x06 },
+	{ "panic",			0x21 },
+	{ NULL,				0x20 },
+	{}
+};
+
+#define RESTART_REASON_PANIC  6
+#define RESTART_REASON_NORMAL 7
 
 static int qcom_reboot_reason_reboot(struct notifier_block *this,
 				     unsigned long event, void *ptr)
@@ -68,13 +85,19 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 		struct qcom_reboot_reason, reboot_nb);
 	struct poweroff_reason *reason;
 
-	if (!cmd)
+	if (!cmd){
+		nvmem_cell_write(reboot->nvmem_cell,
+				 &reasons[RESTART_REASON_NORMAL].pon_reason,
+				 sizeof(reasons[RESTART_REASON_NORMAL].pon_reason));
 		return NOTIFY_OK;
 
 	if (of_device_is_compatible(reboot->dev->of_node, "qcom,imem-reboot-reason"))
 		reboot_mode = REBOOT_WARM;
 
 	for (reason = reboot->reasons; reason->cmd; reason++) {
+	}
+
+	for (reason = reasons; reason->cmd; reason++) {
 		if (!strcmp(cmd, reason->cmd)) {
 			rc = nvmem_cell_write(reboot->nvmem_cell,
 					 &reason->pon_reason,
@@ -82,10 +105,27 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 			if (rc < 0)
 				pr_err("PON reason store failed, rc=%d\n", rc);
 			break;
+					 sizeof(reason->pon_reason));
+			return NOTIFY_OK;
 		}
+
 	}
+	nvmem_cell_write(reboot->nvmem_cell,
+			&reason->pon_reason,
+			sizeof(reason->pon_reason));
 
 	return NOTIFY_OK;
+}
+
+static int panic_prep_restart(struct notifier_block *this,
+			      unsigned long event, void *ptr)
+{
+	struct qcom_reboot_reason *reboot = container_of(this,
+		struct qcom_reboot_reason, panic_nb);
+	nvmem_cell_write(reboot->nvmem_cell,
+			&reasons[RESTART_REASON_PANIC].pon_reason,
+			sizeof(reasons[RESTART_REASON_PANIC].pon_reason));
+	return NOTIFY_DONE;
 }
 
 static int qcom_reboot_reason_probe(struct platform_device *pdev)
@@ -121,6 +161,10 @@ static int qcom_reboot_reason_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, reboot);
 
+  	reboot->panic_nb.notifier_call = panic_prep_restart;
+	reboot->panic_nb.priority = INT_MAX;
+	atomic_notifier_chain_register(&panic_notifier_list, &reboot->panic_nb);
+
 	return 0;
 }
 
@@ -128,6 +172,7 @@ static int qcom_reboot_reason_remove(struct platform_device *pdev)
 {
 	struct qcom_reboot_reason *reboot = platform_get_drvdata(pdev);
 
+	atomic_notifier_chain_unregister(&panic_notifier_list, &reboot->panic_nb);
 	unregister_reboot_notifier(&reboot->reboot_nb);
 
 	return 0;
