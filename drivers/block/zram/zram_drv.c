cf66622947d6c15eb0f9ce86bec2c648530906bf
yumeerin <chan.misaki.y@gmail.com>
2026-08-17 20:53:04 +0000

    driver: zram: fix broken memory-extension writeback feature
    Commit 4bb0de353284 dropped the definitions used by the Xiaomi pid-based
    writeback feature (PID_WB_SIG, LIMIT_WB_SIG, PID_WRITEBACK, struct
    zram_wb_store_walk and writeback_store_apply) while keeping the code that
    references them, and accidentally turned writeback_store_apply into a
    duplicate writeback_store. Restore the missing definitions and revert the
    worker to writeback_store_apply so the driver compiles.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -640,11 +640,20 @@ static int read_from_bdev_async(struct zram *zram, struct bio_vec *bvec,
 }
 
 #define PAGE_WB_SIG "page_index="
+#define PID_WB_SIG  "pid="
+#define LIMIT_WB_SIG "limit="
 
 #define PAGE_WRITEBACK 0
 #define HUGE_WRITEBACK (1<<0)
 #define IDLE_WRITEBACK (1<<1)
+#define PID_WRITEBACK  (1<<15) /* Not much use of this as of today */
 
+struct zram_wb_store_walk {
+	struct zram *zram;
+	u64 limit; /* In page size of 4k */
+	swp_entry_t *entries; /* for batch pte processing */
+	struct mm_struct *mm;
+};
 
 #define INIT_ZRAM_WRITEBACK_STORE(name, zram)			\
 	struct zram_wb_store_walk name = {			\
@@ -690,36 +699,15 @@ static bool is_eligible_swp_to_disk_entry(swp_entry_t swp_entry)
 	return true;
 }
 
-static ssize_t writeback_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t len)
+static ssize_t writeback_store_apply(struct zram *zram, unsigned long index,
+					unsigned long nr_pages, int mode)
 {
-	struct zram *zram = dev_to_zram(dev);
-	unsigned long nr_pages = zram->disksize >> PAGE_SHIFT;
-	unsigned long index = 0;
 	struct bio bio;
 	struct bio_vec bio_vec;
 	struct page *page;
-	ssize_t ret = len;
-	int mode, err;
+	ssize_t ret = 0;
+	int err;
 	unsigned long blk_idx = 0;
-
-	if (sysfs_streq(buf, "idle"))
-		mode = IDLE_WRITEBACK;
-	else if (sysfs_streq(buf, "huge"))
-		mode = HUGE_WRITEBACK;
-	else if (sysfs_streq(buf, "huge_idle"))
-		mode = IDLE_WRITEBACK | HUGE_WRITEBACK;
-	else {
-		if (strncmp(buf, PAGE_WB_SIG, sizeof(PAGE_WB_SIG) - 1))
-			return -EINVAL;
-
-		if (kstrtol(buf + sizeof(PAGE_WB_SIG) - 1, 10, &index) ||
-				index >= nr_pages)
-			return -EINVAL;
-
-		nr_pages = 1;
-		mode = PAGE_WRITEBACK;
-	}
 
 	down_read(&zram->init_lock);
 	if (!init_done(zram)) {
